18081e588c8a721023a3cdb1ec7780d86e2d5650
elaa <elaa.brahmii@gmail.com>
2025-06-07 00:01:28 +0100

    loaded manually .env file

--- backend-copy/src/main/java/backend/example/demo/DemoApplication.java
+++ backend-copy/src/main/java/backend/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package backend.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		// Set them as system properties (Spring will pick these up)
+		System.setProperty("CLIENT_ID", dotenv.get("CLIENT_ID"));
+		System.setProperty("CLIENT_SECRET", dotenv.get("CLIENT_SECRET"));
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
