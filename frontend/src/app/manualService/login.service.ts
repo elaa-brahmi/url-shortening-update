0524a66d318fcbdd1e4cee4097ba33aa97d3eb62
elaa <elaa.brahmii@gmail.com>
2025-06-07 23:55:21 +0100

    add logout

--- frontend/src/app/manualService/login.service.ts
+++ frontend/src/app/manualService/login.service.ts
@@ -1,18 +1,26 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
   private basePath = 'http://localhost:8080'; // or from environment.ts
+   constructor(private http: HttpClient,private router:Router,private route:ActivatedRoute) {}
 
-   constructor(private http: HttpClient) {}
 
    login(): void {
      window.location.href = `${this.basePath}/login/google`;
-     //const params = new HttpParams().set('shortUrl', shortUrl); // for @requestParam
+   }
+   logout(){
+    //delete token
+    localStorage.removeItem('token');
+    localStorage.removeItem('name');
+    localStorage.removeItem('id');
+    this.router.navigate(['/login']);
+
+
    }
 }
